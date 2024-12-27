0bee09538349ed5c77522fa016eafea9020c3c4f
TejaswiMahadev <151984853+TejaswiMahadev@users.noreply.github.com>
2024-12-27 19:09:54 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,7 +6,6 @@ import zipfile
 import pandas as pd 
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-from movie import *
 import requests
 from concurrent.futures import ThreadPoolExecutor
 
